6836d6cb8ebe304774a3f0f3d390496aa8bfbd0e
RlxChap2 <mmah02778@gmail.com>
2025-04-13 20:15:28 +0300

    V1.2.0

--- src/components/EasterEgg.tsx
+++ src/components/EasterEgg.tsx
@@ -12,7 +12,7 @@ export const EasterEgg: React.FC<EasterEggProps> = ({ show, onClose }) => {
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50" onClick={onClose}>
             <div className="relative w-full max-w-4xl bg-black rounded-xl overflow-hidden">
                 <video autoPlay controls className="w-full" onClick={(e) => e.stopPropagation()}>
-                    <source src="./quiz-master/easteregg.mp4" type="video/mp4" />
+                    <source src="./easteregg.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
